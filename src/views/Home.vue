7cafc1e94c3d48f3fdc0f03d301869cebc6d112d
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-04-01 00:12:24 +0200

    Prvi komit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="first"
+            label="First Name"
+            filled
+          ></v-text-field>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </v-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
